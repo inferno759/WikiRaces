777beb19768e4e5fd6da5900a5899d105e288ddb
Exsolutus <exsolutus.dev@gmail.com>
2021-03-15 17:02:18 -0400

    Added named foreign key constraints to SQL script.

--- Project2-CreationScript.sql
+++ Project2-CreationScript.sql
@@ -11,6 +11,7 @@ DROP TABLE Races;
 DROP TABLE Users;
 
 */
+
 CREATE TABLE Users (
 	ID int IDENTITY(1,1) PRIMARY KEY,
 	Username nvarchar(25) NOT NULL,
@@ -19,35 +20,41 @@ CREATE TABLE Users (
 
 CREATE TABLE Races (
 	ID int IDENTITY(1,1) PRIMARY KEY,
-	AuthorID int FOREIGN KEY REFERENCES Users(ID),
+	AuthorID int,
 	Title nvarchar(100),
 	Type nvarchar(50),
 	TimeLimit float,
 	StepLimit int,
 	StartPage varchar(1000),
-	EndPage varchar(1000)
+	EndPage varchar(1000),
+	CONSTRAINT FK_Races_AuthorId FOREIGN KEY (AuthorId) REFERENCES Users
 );
 
 CREATE TABLE Leaderboard (
 	ID int IDENTITY(1,1) PRIMARY KEY,
-	RaceID int FOREIGN KEY REFERENCES Races(ID),
-	UserID int FOREIGN KEY REFERENCES Users(ID),
+	RaceID int,
+	UserID int,
 	Score int,
 	TimeElapsed float,
 	StepsTaken int,
-	CompletionDate Datetime
+	CompletionDate Datetime,
+	CONSTRAINT FK_Leaderboard_Races FOREIGN KEY (RaceID) REFERENCES Races,
+	CONSTRAINT FK_Leaderboard_Users FOREIGN KEY (UserId) REFERENCES Users
 );
 
 CREATE TABLE PathStep (
 	ID int IDENTITY(1,1) PRIMARY KEY,
-	LeaderboardID int FOREIGN KEY REFERENCES Leaderboard(ID),
+	LeaderboardID int,
 	CurrentPage varchar(1000),
 	StepNumber int,
-	TimeSpent float
+	TimeSpent float,
+	CONSTRAINT FK_PathStep_Leaderboard FOREIGN KEY (LeaderboardID) REFERENCES Leaderboard
 );
 
 CREATE TABLE Friends (
 	ID int IDENTITY(1,1) PRIMARY KEY,
-	UserID int FOREIGN KEY REFERENCES Users(ID),
-	FriendID int FOREIGN KEY REFERENCES Users(ID),
+	UserID int,
+	FriendID int,
+	CONSTRAINT FK_Friends_User FOREIGN KEY (UserID) REFERENCES Users,
+	CONSTRAINT FK_Friends_Friend FOREIGN KEY (FriendID) REFERENCES Users
 );	
