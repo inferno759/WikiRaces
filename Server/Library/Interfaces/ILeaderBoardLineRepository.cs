e27fb5a39393e9591e6890fa93b7ec99c9793438
Trevor Dunbar <tdunbar123@yahoo.com>
2021-03-17 17:55:06 -0400

    Wrote interfaces for repositories

---
+++ Server/Library/Interfaces/ILeaderBoardLineRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Model;
+
+namespace Library.Interfaces
+{
+    public interface ILeaderBoardLineRepository
+    {
+        Task AddLeaderboardLine(LeaderboardLine leaderboardLine);
+        Task<LeaderboardLine> GetLeaderboardLineByID(int id);
+        Task<IEnumerable<LeaderboardLine>> GetLeaderboardLines();
+        Task<IEnumerable<LeaderboardLine>> GetLeaderboardLinesByUser(User user);
+        Task<IEnumerable<LeaderboardLine>> GetLeaderboardLinesByRace(Race race);
+        Task UpdateLeaderBoardLine(LeaderboardLine leaderboardLine);
+        Task DeleteLeaderBoardLine(LeaderboardLine leaderboardLine);
+    }
+}
---
+++ Server/Library/Interfaces/IRaceRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Model;
+
+namespace Library.Interfaces
+{
+    public interface IRaceRepository
+    {
+        Task AddRace(Race race);
+        Task<Race> GetRaceByID(int id);
+        Task<IEnumerable<Race>> GetRaces();
+        Task<IEnumerable<Race>> GetRacesByTitle(string title);
+        Task UpdateRace(Race race);
+        Task DeleteRace(Race race);
+    }
+}
