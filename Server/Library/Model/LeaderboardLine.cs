24d2fd949d977d5752db4b788e0939e2a6488b1e
Caleb Owens <owenscaleb75@gmail.com>
2021-03-17 16:18:25 -0500

    creation of LeaderboardLine model

--- Server/Library/Model/LeaderboardLine.cs
+++ Server/Library/Model/LeaderboardLine.cs
@@ -9,5 +9,64 @@ namespace Library.Model
     public class LeaderboardLine
     {
 
+        private int _score;
+        private float _timeElapsed;
+        private int _stepsTaken;
+        public int Id { get; set; }
+
+        // race ID foreign key done at Repo add
+
+        // user ID foreign key done at Repo add
+
+        public int Score
+        {
+            get { return _score; }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Score shouldn't be negative or 0");
+                }
+                else
+                {
+                    _score = value;
+                }
+            }
+        }
+        public float TimeElapsed
+        {
+            get { return _timeElapsed; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("TimeElapsed should not be negative or 0");
+                }
+                else
+                {
+                    _timeElapsed = value;
+                }
+            }
+        }
+        public int StepsTaken
+        {
+            get { return _stepsTaken; }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("StepsTaken should not be negative or 0");
+                }
+                else
+                {
+                    _stepsTaken = value;
+                }
+            }
+        }
+        public DateTime LeaderboardDateTime { get; set; }
+
+
+
+
     }
 }
