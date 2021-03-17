d7a70c2d8ed784bf94f1b69fd46db9db4cfd0b10
Trevor Dunbar <tdunbar123@yahoo.com>
2021-03-16 23:50:57 -0400

    Implemented user and tests

--- Server/Library/Model/User.cs
+++ Server/Library/Model/User.cs
@@ -8,9 +8,88 @@ namespace Library.Model
 {
     public class User
     {
+        private string _username;
+
+        private string _password;
+        public int Id { get; set; }
+
+        public string Username 
+        { 
+            get { return _username; } 
+            set
+            {
+                if(value.Length < 3)
+                {
+                    throw new ArgumentException("Username must be at least 3 characters long");
+                }
+                else if(value.Length > 25)
+                {
+                    throw new ArgumentException("Username cannot be longer than 25 characters");
+                }
+                foreach(Char c in value)
+                {
+                    if (!Char.IsLetterOrDigit(c))
+                    {
+                        throw new ArgumentException("Username can only contain letters and/or numbers");
+                    }
+                }
+                _username = value;
+            }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (value.Length < 8)
+                {
+                    throw new ArgumentException("Password must be at least 8 characters long");
+                }
+                else if (value.Length > 25)
+                {
+                    throw new ArgumentException("Password cannot be longer than 25 characters");
+                }
+                foreach (Char c in value)
+                {
+                    if (Char.IsWhiteSpace(c))
+                    {
+                        throw new ArgumentException("Password cannot contain spaces");
+                    }
+                }
+                _password = value;
+            }
+        }
+        public List<User> Friends { get; set; }
+
         public User()
         {
 
+        }
+
+        public User(int id, string username, string password)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            Friends = new List<User>();
+        }
+
+        public void AddFriend(User friend)
+        {
+            if (Friends.Contains(friend))
+            {
+                throw new ArgumentException("Friend to add already exists");
+            }
+            Friends.Add(friend);
+        }
+
+        public void DeleteFriend(User friend)
+        {
+            if (!Friends.Contains(friend))
+            {
+                throw new ArgumentException("Friend to delete doesn't exist");
+            }
+            Friends.Remove(friend);
         }
     }
 }
--- Server/Test/UserTest.cs
+++ Server/Test/UserTest.cs
@@ -9,12 +9,145 @@ namespace Tests
     public class UserTest
     {
         [Fact]
-        public void UserConstructedPassed()
+        public void UserConstructorPassed()
         {
+            int id = 1;
+            string username = "tdunbar";
+            string password = "password123!";
+
+            User user = new User(id, username, password);
+
+            Assert.Equal(1, user.Id);
+            Assert.Equal("tdunbar", user.Username);
+            Assert.Equal("password123!", user.Password);
+        }
+
+        [Fact]
+        public void UserUsernameTooShort()
+        {
+            string username = "td";
 
             User user = new User();
 
-            Assert.NotNull(user);
+            Assert.Throws<ArgumentException>(() => user.Username = username);
+        }
+
+        [Fact]
+        public void UserUsernameTooLong()
+        {
+            string username = "12345678901234567890123456"; // 26 chars
+
+            User user = new User();
+
+            Assert.Throws<ArgumentException>(() => user.Username = username);
+        }
+
+        [Fact]
+        public void UserUsernameNotAlphaNumeric()
+        {
+            string username = "tdunb@r";
+
+            User user = new User();
+
+            Assert.Throws<ArgumentException>(() => user.Username = username);
+        }
+
+        [Fact]
+        public void UserPasswordTooShort()
+        {
+            string password = "td";
+
+            User user = new User();
+
+            Assert.Throws<ArgumentException>(() => user.Password = password);
+        }
+
+        [Fact]
+        public void UserPasswordTooLong()
+        {
+            string password = "12345678901234567890123456"; // 26 chars
+
+            User user = new User();
+
+            Assert.Throws<ArgumentException>(() => user.Password = password);
+        }
+
+        [Fact]
+        public void UserPasswordContainsWhitespace()
+        {
+            string password = "tdunb r";
+
+            User user = new User();
+
+            Assert.Throws<ArgumentException>(() => user.Password = password);
+        }
+
+        [Fact]
+        public void UserAddFriendSuccess()
+        {
+            int id = 1;
+            string username = "tdunbar";
+            string password = "password123!";
+            int friendId = 2;
+            string friendUsername = "brandonF";
+            string friendPassword = "!321drowssap";
+
+            User user = new User(id, username, password);
+            User friend = new User(friendId, friendUsername, friendPassword);
+            user.AddFriend(friend);
+
+            Assert.Contains<User>(friend, user.Friends);
+        }
+
+        [Fact]
+        public void UserAddFriendFails()
+        {
+            int id = 1;
+            string username = "tdunbar";
+            string password = "password123!";
+            int friendId = 2;
+            string friendUsername = "brandonF";
+            string friendPassword = "!321drowssap";
+
+            User user = new User(id, username, password);
+            User friend = new User(friendId, friendUsername, friendPassword);
+            user.AddFriend(friend);
+
+            Assert.Throws<ArgumentException>(() => user.AddFriend(friend));
+        }
+
+        [Fact]
+        public void UserDeleteFriendSuccess()
+        {
+            int id = 1;
+            string username = "tdunbar";
+            string password = "password123!";
+            int friendId = 2;
+            string friendUsername = "brandonF";
+            string friendPassword = "!321drowssap";
+
+            User user = new User(id, username, password);
+            User friend = new User(friendId, friendUsername, friendPassword);
+            user.AddFriend(friend);
+            user.DeleteFriend(friend);
+
+            Assert.DoesNotContain<User>(friend, user.Friends);
+        }
+
+        [Fact]
+        public void UserDeleteFriendFails()
+        {
+            int id = 1;
+            string username = "tdunbar";
+            string password = "password123!";
+            int friendId = 2;
+            string friendUsername = "brandonF";
+            string friendPassword = "!321drowssap";
+
+            User user = new User(id, username, password);
+            User friend = new User(friendId, friendUsername, friendPassword);
+
+            Assert.Throws<ArgumentException>(() => user.DeleteFriend(friend));
         }
     }
 }
