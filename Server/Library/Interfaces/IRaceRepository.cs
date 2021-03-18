35b0b6398b805689aec146ab0f4223f4ee15c467
Caleb Owens <owenscaleb75@gmail.com>
2021-03-17 23:12:21 -0500

    RaceRepository Add and GetAll

--- Server/Library/Interfaces/IRaceRepository.cs
+++ Server/Library/Interfaces/IRaceRepository.cs
@@ -5,13 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using Library.Model;
 
+
 namespace Library.Interfaces
 {
     public interface IRaceRepository
     {
         Task AddRace(Race race);
         Task<Race> GetRaceByID(int id);
-        Task<IEnumerable<Race>> GetRaces();
+        Task<List<Race>> GetRaces();
         Task<IEnumerable<Race>> GetRacesByTitle(string title);
         Task UpdateRace(Race race);
         Task DeleteRace(Race race);
