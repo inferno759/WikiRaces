f84c98418e1b1885427da238133494339b70510d
Exsolutus <exsolutus.dev@gmail.com>
2021-03-19 10:39:57 -0400

    Added races component, race service, etc. for dashboard to reference.

--- Client/Project2-Angular/src/app/app.module.ts
+++ Client/Project2-Angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RacesComponent } from './races/races.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    RacesComponent
   ],
   imports: [
     BrowserModule,
