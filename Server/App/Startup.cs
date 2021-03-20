9da9d01f4d717d0a9cf4b968c46238171e043cdd
Trevor Dunbar <tdunbar123@yahoo.com>
2021-03-20 01:20:27 -0400

    Testing redirect

--- Server/App/Startup.cs
+++ Server/App/Startup.cs
@@ -47,7 +47,7 @@ namespace WikiRacing
             services.AddScoped<Library.Interfaces.ILeaderBoardLineRepository, Data.Repositories.LeaderboardLineRepository>();
 
             services.AddCors(options => options.AddDefaultPolicy(config => config
-             .WithOrigins("http://localhost:4200", "https://team4-project2.azurewebsites.net/")
+             .WithOrigins("http://localhost:4200", "https://team4-project2-client.azurewebsites.net/")
              .AllowAnyMethod()
              .AllowAnyHeader()
              .AllowCredentials()));
@@ -71,12 +71,10 @@ namespace WikiRacing
             app.UseStatusCodePages();
 
             app.UseHttpsRedirection();
-
-            /* change index.html to the mainpage
               
             app.UseRewriter(new RewriteOptions()
-                .AddRedirect("^$", "index.html"));
-            */
+                .AddRedirect("^$", "dashboard"));
+            
             app.UseStaticFiles();
             app.UseRouting();
 
