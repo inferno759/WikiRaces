2a8c7a8e330c75eb077de26c188369cadc27fd08
Exsolutus <exsolutus.dev@gmail.com>
2021-03-20 13:41:09 -0400

    Attempt to have race service send requests to the Azure hosted server app.

--- Client/Project2-Angular/src/app/race.service.ts
+++ Client/Project2-Angular/src/app/race.service.ts
@@ -9,7 +9,7 @@ import { Race } from './race';
   providedIn: 'root'
 })
 export class RaceService {
-  private racesUrl = 'api/race';  // URL to web api
+  private racesUrl = 'https://team4-project2.azurewebsites.net/dashboard/api/race';  // URL to web api
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
