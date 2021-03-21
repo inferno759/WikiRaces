4a63e5944ea3b78166b51b261310f696beb3a508
Caleb Owens <owenscaleb75@gmail.com>
2021-03-21 17:24:55 -0500

    Web Scraper controller and methods as well as HTML Agility pack added to ASP

---
+++ Server/App/Controllers/WebScraperController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+
+namespace App.Controllers
+{
+    //[Route("api/[controller]")]
+
+    [ApiController]
+    public class WebScraperController : ControllerBase
+    {
+
+
+
+        [HttpGet("api/webscraper/{url}")]
+        public IActionResult ControllerGetLinks(string url)
+        {
+            var response =  CallUrl(url).Result;
+            var linkList =  ParseHtml(response);
+
+            return Ok(linkList);
+        }
+
+        private static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = client.GetStringAsync(fullUrl);
+            return await response;
+        }
+
+        private static List<string> ParseHtml(string html)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            var htmlBody = doc.DocumentNode.SelectSingleNode("//div[@id='bodyContent']");
+            var htmlLinks = htmlBody.Descendants("a").ToList();
+
+            List<string> wikiLinks = new List<string>();
+
+            foreach(var link in htmlLinks)
+            {
+                wikiLinks.Add(link.GetAttributeValue("href", ""));
+            }
+
+            return wikiLinks;
+            
+
+        }
+    }
+}
